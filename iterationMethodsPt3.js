c5bd55f608b233b326b552c3107792d8084a8333
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-08-08 08:48:50 +0300

    clean the code

--- iterationMethodsPt3.js
+++ iterationMethodsPt3.js
@@ -7,29 +7,27 @@ const transactions = [
 ];
 
 const onlyIncomeTansactions = transactions.filter((transaction)=> transaction[0] == "income");
-
 const onlyExpenseTansactions = transactions.filter((transaction)=> transaction[0] == "expense");
 
 
-
 let totalIn = 0;
+let totalEx = 0;
 
 onlyIncomeTansactions.forEach((income)=>totalIn+=income[1]);
-
 console.log("Total Income: ",totalIn);
 
-totalEx = 0;
 
 onlyExpenseTansactions.forEach((expense)=>totalEx+=expense[1]);
-
 console.log("Total Expense: ",totalEx);
 
-const totalNet = (ex,inc)=>{return ex-inc};
+console.log("Only Income Transactions",onlyIncomeTansactions)
+console.log("Only Expense Transactions",onlyExpenseTansactions)
 
+
+const totalNet = (ex,inc)=>{return ex-inc};
 console.log("Total Net: ",totalNet(totalIn, totalEx))
 
 const transactionsAbove500 = transactions.filter((transaction)=>transaction[1] > 500)
-
 console.log("Transactions above 500: ",transactionsAbove500);
 
 
